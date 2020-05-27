a0d1423fa865ef95a738f4089435cbca48e52a6e
Willson Ngo <wngo@grubhub.com>
2020-05-27 16:36:27 -0400

    add footer

--- src/components/footer.jsx
+++ src/components/footer.jsx
@@ -1,11 +1,31 @@
 import React from "react";
+import StickyFooter from 'react-sticky-footer';
+import "./footer.css"
 
-function Footer() {
+const FooterPage = () => {
     return (
-        <div>
-            <h1>This is footer</h1>
-        </div>
+        <StickyFooter
+            bottomThreshold={50}
+            normalStyles={{
+                backgroundColor: "#F8F9F9",
+                padding: "2rem"
+            }}
+            stickyStyles={{
+                backgroundColor: "rgba(255,255,255,.8)",
+                padding: "2rem"
+            }}
+        >
+
+            <div align="center">
+                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
+
+                <a href="https://www.facebook.com/ntson" className="fa fa-facebook"></a>
+                <a href="https://www.youtube.com/" className="fa fa-youtube"></a>
+                <a href="https://www.linkedin.com/in/wilson-ngo-7984ba34/" className="fa fa-linkedin"></a>
+                <a href="https://www.youtube.com/" className="fa fa-instagram"></a>
+            </div>
+        </StickyFooter>
     );
 }
 
-export default Footer;
+export default FooterPage;

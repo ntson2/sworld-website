f7f183f81e938b6f5cea88b5d9ddeb5ddd72c736
dang soan <doanquangbao.it@gmail.com>
2020-06-08 23:03:05 +0700

    Change condition to animated header and muted sound video

--- src/share/scroll-handler/scroll-handler.js
+++ src/share/scroll-handler/scroll-handler.js
@@ -1,14 +1,15 @@
 import React, {useState, useEffect} from 'react';
 import {FaArrowCircleUp} from 'react-icons/fa';
-import { red } from '@material-ui/core/colors';
-import zIndex from '@material-ui/core/styles/zIndex';
+import * as _ from 'lodash';
 
-export const useScrollHandler = () => {
+
+export const useScrollHandler = (props) => {
     const [scroll, setScroll] = useState(1)
+    const offsetY = _.cloneDeep(props);
 
     useEffect(() => {
         const onScroll = () => {
-            const scrollCheck = window.scrollY < 700
+            const scrollCheck = window.scrollY < offsetY;
             if (scrollCheck !== scroll) {
                 setScroll(scrollCheck)
             }
